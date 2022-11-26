67a9f321c0b807112cac3347167ced34bdf97f55
fahricandurucan <fahricandurucan@icloud.com>
2022-11-27 01:44:12 +0300

    burc detay sayfasındaki appbar a değişken renkler atandı

--- lib/BurcDetay.dart
+++ lib/BurcDetay.dart
@@ -2,10 +2,30 @@ import 'package:flutter/material.dart';
 
 import 'model/burc.dart';
 
-class BurcDetay extends StatelessWidget {
+import 'package:palette_generator/palette_generator.dart';
+
+
+class BurcDetay extends StatefulWidget {
 
   late Burc secilenBurc;
   BurcDetay({required this.secilenBurc});
+
+
+  @override
+  _BurcDetayState createState() => _BurcDetayState();
+}
+
+class _BurcDetayState extends State<BurcDetay> {
+
+  Color appbarRengi = Colors.pink;
+  late PaletteGenerator generator ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    appbarRenkAyarla();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,17 +35,32 @@ class BurcDetay extends StatelessWidget {
           SliverAppBar(
             expandedHeight: 250,//uygulama açıldığında ne kadarlık yer tutacağını belirledik
             pinned: true, //sabit bi şekilde kalması için yukarı aşağı hareket ettirdiğimizde
-            title: Text(secilenBurc.burc_ad),
-            backgroundColor: Colors.pink,
-            flexibleSpace: FlexibleSpaceBar(background: Image.asset(secilenBurc.burc_buyuk_resim)),
+
+            backgroundColor: appbarRengi,
+            flexibleSpace: FlexibleSpaceBar(
+              background: Image.asset(widget.secilenBurc.burc_buyuk_resim,fit: BoxFit.cover,),
+              title: Text(widget.secilenBurc.burc_ad + "Burcu ve Özellikleri"),
+              centerTitle: true,
+            ),
           ),
           SliverToBoxAdapter( //slşver olmayan bir şeyler ekleyebilmek için bu widget içine yazmalıyız
             child: SingleChildScrollView(
-              child: Text(secilenBurc.burc_detay,style: TextStyle(fontSize: 18),),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Text(widget.secilenBurc.burc_detay,style: TextStyle(fontSize: 18),),
+              ),
             ),
           )
         ],
       ),
     );
+  }
+
+  void appbarRenkAyarla() async{ //appar rengi değişken hale getirdik(palette_generator kütüphanesini kullanarak)
+    generator = await PaletteGenerator.fromImageProvider(AssetImage(widget.secilenBurc.burc_buyuk_resim));
+    setState(() {
+      appbarRengi = generator.dominantColor!.color;
+    });
+
   }
 }
